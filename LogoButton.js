1e98e8f4dfdb75f0bbef8e526b89cf545d031c3b
adriannic <spamadriannic@gmail.com>
2023-12-08 02:41:30 +0100

    LogoButton now opens wofi

--- LogoButton.js
+++ LogoButton.js
@@ -1,4 +1,5 @@
 import Widget from "resource:///com/github/Aylur/ags/widget.js";
+import { execAsync } from "resource:///com/github/Aylur/ags/utils.js";
 import { lookUpIcon } from "resource:///com/github/Aylur/ags/utils.js";
 
 export const LogoButton = () =>
@@ -8,6 +9,9 @@ export const LogoButton = () =>
     children: [
       Widget.Button({
         className: "container",
+        onPrimaryClick: () =>
+          execAsync(["bash", "-c", "killall wofi || wofi"]).then(() => { })
+            .catch(console.error),
         child: lookUpIcon("start-here-archlinux")
           ? Widget.Icon({ icon: "start-here-archlinux", size: 21 })
           : Widget.Label({ label: "", style: "padding-right: 6px;" }),
