7400f618c5d0aab7b2320791664c4f81a1efdd05
adriannic <nicolas.aguilera.adrian@gmail.com>
2024-12-06 15:05:24 +0100

    feat(workspaces): fix workspace passive updates

--- widget/Workspaces.tsx
+++ widget/Workspaces.tsx
@@ -1,27 +1,35 @@
 import Gtk from "gi://Gtk?version=3.0";
-import { Variable } from "../../../../../usr/share/astal/gjs";
+import Hyprland from "gi://AstalHyprland";
+import { Box } from "astal/gtk3/widget";
 import Settings from "./Settings";
 
-const focusedWorkspace = Variable("{}").poll(150, "hyprctl monitors -j");
+const hypr = Hyprland.get_default();
+
 function IndicatorWidget({ monitor }: { monitor: string; }) {
+  const updateWorkspace: (self: Box, event: string) => void = (self, event) => {
+    if (event !== "workspacev2")
+      return;
+
+    const monitors = hypr.get_monitors();
+    const selectedWorkspace = monitors[parseInt(monitor)].activeWorkspace.id;
+    self.visible = selectedWorkspace < 10;
+
+    const marginLeft = (selectedWorkspace - 1) * 30 + 4;
+    const marginRight = (Settings.workspaceList.length - selectedWorkspace) * 30 + 4;
+    self.css = `
+                        margin-left: ${marginLeft}px;
+                        margin-right: ${marginRight}px;
+                        transition: margin ${Settings.ANIMATION_SPEED_IN_MILLIS}ms ease-in-out;`;
+  };
+
   return <box>
     <box
       hexpand
       vexpand
       className="selectedWorkspace"
       setup={self => {
-        self.hook(focusedWorkspace, (self) => {
-          const monitors = JSON.parse(focusedWorkspace.get());
-          const selectedWorkspace = monitors[parseInt(monitor)].activeWorkspace.id;
-          self.visible = selectedWorkspace < 10;
-
-          const marginLeft = (selectedWorkspace - 1) * 30 + 4;
-          const marginRight = (Settings.workspaceList.length - selectedWorkspace) * 30 + 4;
-          self.css = `
-                        margin-left: ${marginLeft}px;
-                        margin-right: ${marginRight}px;
-                        transition: margin ${Settings.ANIMATION_SPEED_IN_MILLIS}ms ease-in-out;`;
-        });
+        updateWorkspace(self, "workspacev2");
+        self.hook(hypr, "event", updateWorkspace);
       }} />
   </box>;
 }
