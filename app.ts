0c75bc3658773d5e17016023054feec4fc6f36d5
adriannic <nicolas.aguilera.adrian@gmail.com>
2025-02-04 05:00:30 +0100

    fix: multimonitor setup not detecting new monitors

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/ags run
 
-import { App } from "astal/gtk3"
+import { App, Gdk, Gtk } from "astal/gtk3"
 import { exec } from "astal/process"
 import Bar from "./widget/Bar"
 import Spacer from "./widget/Spacer"
@@ -12,9 +12,37 @@ exec("sass ./style.scss /tmp/style.css");
 App.start({
     css: "/tmp/style.css",
     main() {
-        App.get_monitors().map(Spacer);
-        App.get_monitors().map(Bar);
-        App.get_monitors().map(Background);
-        App.get_monitors().map(Notifications);
+        const spacers = new Map<Gdk.Monitor, Gtk.Widget>();
+        const bars = new Map<Gdk.Monitor, Gtk.Widget>();
+        const bgs = new Map<Gdk.Monitor, Gtk.Widget>();
+        const notifications = new Map<Gdk.Monitor, Gtk.Widget>();
+
+        App.get_monitors().forEach((gdkmonitor, id) => {
+            spacers.set(gdkmonitor, Spacer(gdkmonitor, id));
+            bars.set(gdkmonitor, Bar(gdkmonitor, id));
+            bgs.set(gdkmonitor, Background(gdkmonitor, id));
+            notifications.set(gdkmonitor, Notifications(gdkmonitor));
+        });
+
+        App.connect("monitor-added", (_, gdkmonitor) => {
+            const id = App.get_monitors().indexOf(gdkmonitor);
+
+            spacers.set(gdkmonitor, Spacer(gdkmonitor, id));
+            bars.set(gdkmonitor, Bar(gdkmonitor, id));
+            bgs.set(gdkmonitor, Background(gdkmonitor, id));
+            notifications.set(gdkmonitor, Notifications(gdkmonitor));
+        })
+
+        App.connect("monitor-removed", (_, gdkmonitor) => {
+            spacers.get(gdkmonitor)?.destroy();
+            bars.get(gdkmonitor)?.destroy();
+            bgs.get(gdkmonitor)?.destroy();
+            notifications.get(gdkmonitor)?.destroy();
+
+            spacers.delete(gdkmonitor);
+            bars.delete(gdkmonitor);
+            bgs.delete(gdkmonitor);
+            notifications.delete(gdkmonitor);
+        });
     },
 })
